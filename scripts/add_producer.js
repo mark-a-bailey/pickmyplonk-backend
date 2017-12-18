a2dc98418a111bbc66987c6558fb16266e7a8370
conwayp <paul.conway@integrate.co.uk>
2017-12-18 20:57:26 +0000

    Add producer API

---
+++ scripts/add_producer.js
@@ -0,0 +1,45 @@
+import uuid from "uuid";
+import AWS from "aws-sdk";
+
+AWS.config.update({ region: "eu-west-2" });
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: "producer",
+        Item: {
+            uuid: uuid.v1(),
+            name: data.name
+        }
+    };
+
+    dynamoDb.put(params, (error, data) => {
+        // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        };
+
+    // Return status code 500 on error
+    if (error) {
+        const response = {
+            statusCode: 500,
+            headers: headers,
+            body: JSON.stringify({ status: false })
+        };
+        callback(null, response);
+        return;
+    }
+
+    // Return status code 200 and the newly created item
+    const response = {
+        statusCode: 200,
+        headers: headers,
+        body: JSON.stringify(params.Item)
+    };
+    callback(null, response);
+});
+}
