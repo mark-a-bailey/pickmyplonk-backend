6987c8b21051bda87492c5488e585dc509f730a0
conwayp <paul.conway@integrate.co.uk>
2017-12-30 11:59:25 +0000

    Get producers API

---
+++ scripts/get_producers.js
@@ -0,0 +1,46 @@
+import uuid from "uuid";
+import AWS from "aws-sdk";
+
+//todo: is this required?
+AWS.config.update({ region: "eu-west-2" });
+
+export function main(event, context, callback) {
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(get_producer_list())
+    };
+
+    callback(null, response);
+}
+
+function get_producer_list() {
+
+    let producer_names = [
+        'Wolfblass',
+        'Trivento',
+        'Brancott Estate',
+        'Hardys',
+        'Barefoot',
+        'Blossom Hill',
+        'Lambrini',
+        'Blue Nun',
+        'Mateus',
+        'Yellow Tail',
+        'Macaron',
+        'Cristal',
+        'Camel Valley',
+        'Oxney',
+        'Giffords Hall',
+        'Denbies',
+        'Lyme Bay',
+        'Halfpenny Green',
+        'Sixteen Ridges'
+    ];
+
+    return producer_names.map(function (name) {
+       let producer_uuid = uuid.v1();
+       return {uuid: producer_uuid, name: name};
+    });
+
+}
